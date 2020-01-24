bd09062243da3a862748e8c8be747e5376a483c0
shriniwas <shriniwas@tradesocio.com>
2020-01-24 15:32:50 +0530

    added placeholder json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-test';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe(data => console.log(data));
+  }
 }
